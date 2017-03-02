0388887f7f44e2b0ec666b361fe217c00dfa1b56
Terrance Koar <terrancekoar@gmail.com>
2017-03-02 23:15:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function getFirstSelector(selector) {
-  return document.querySelector('selector');
+  return document.querySelector(selector);
 }
 
 function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
 
+function deepestChild() {
+  return document.querySelector('#grand-node div');
+}
+
+function increaseRankBy(n) {
+  var increase = document.querySelectorAll('.ranked-list');
+  
+  for (var i = 0; i < increase.length; i++) {
+    var childrenToIncrease = increase[i].children; 
+    
+    for (var x = 0; x < childrenToIncrease.length; x++) {
+      childrenToIncrease[x].innerHTML = parseInt(childrenToIncrease[x].innerHTML) + n;
+    }
+  }
 }
